0a0adb2a96fd1b75002808ca4150620521d84f63
Polka-S <gillson11@yandex.ru>
2023-10-01 22:39:36 +0300

    Make module for interacting with player

---
+++ include/user_interaction.h
@@ -0,0 +1,15 @@
+#ifndef USER_INTERACTION_H
+#define USER_INTERACTION_H
+
+#define MAX_SIZE_STR 256
+#define MAX_SIZE_WORD 10
+#define ERROR 1
+#define SUCCESS 0
+
+#include <stdio.h>
+
+#include "user_interaction.h"
+
+void answer(char *finish_word, char *user_word);
+
+#endif  // USER_INTERACTION_H

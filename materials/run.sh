71eecbf3ba75fefe192f312fd5ee66dff0315c07
Polka-S <gillson11@yandex.ru>
2023-10-01 23:00:18 +0300

    Change run.sh

--- materials/run.sh
+++ materials/run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-cp linters/.clang-format ../src/.clang-format
-cp linters/.clang-format ../include/.clang-format
-
-clang-format ../include/*.h ../src/*.c -i
+clang-format --style=file:linters/.clang-format ../include/*.h ../src/*.c -i
 
 cppcheck --enable=all --suppress=missingIncludeSystem -I ../include ../src
 

5954d42639b8439dec19556a76244b08dc8dad85
Polka-S <gillson11@yandex.ru>
2023-10-02 13:20:32 +0300

    Added the definition of functions for tracking logs

--- include/logger.h
+++ include/logger.h
@@ -1,4 +1,22 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "log_levels.h"
+
+typedef struct {
+    FILE *log_file;
+    enum log_level current_level;
+} Logger;
+
+Logger *log_init(const char *name_file, enum log_level level);
+
+void logcat(enum log_level level, const char *format, ...);
+
+void logclose();
+
 #endif  // LOGGER_H

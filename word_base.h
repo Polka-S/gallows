a451991a7c40aa36830de4dee86bb5b1ff70d13a
Polka-S <gillson11@yandex.ru>
2023-10-02 12:04:57 +0300

    Scattered files in directories

--- word_base.h
+++
@@ -1,9 +0,0 @@
-#ifndef GALLOWS_WORD_BASE_H
-#define GALLOWS_WORD_BASE_H
-
-char* random_easy_word();
-char* random_medium_word();
-char* random_hard_word();
-
-void start_random_generator();
-#endif //GALLOWS_WORD_BASE_H

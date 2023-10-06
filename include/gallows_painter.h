4074d84bf407a9f45f471cfc4ce87618aecc02e0
kot_eugene <to_ev@mail.ru>
2023-10-06 12:18:30 +0300

    Fixed gallows_painter.c

--- include/gallows_painter.h
+++ include/gallows_painter.h
@@ -1,6 +1,16 @@
 #ifndef UNTITLED_GALLOWS_PAINTER_H
 #define UNTITLED_GALLOWS_PAINTER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "config.h"
+
+#define MAX_PARAMETER_LEN 64
+#define MAX_STRING_LEN 64
+#define LEFT_MARGIN "     "
+
 void draw_gallows(int elems);
 
-#endif //UNTITLED_GALLOWS_PAINTER_H
+#endif  // UNTITLED_GALLOWS_PAINTER_H
